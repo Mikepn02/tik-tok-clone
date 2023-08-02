6a34546101f23c6bfe5cc221d0767a666d952dca
Mikepn02 <121401821+Mikepn02@users.noreply.github.com>
2023-08-03 01:56:14 +0200

    another update

---
+++ pages/api/post/index.ts
@@ -0,0 +1,18 @@
+import type { NextApiRequest , NextApiResponse } from "next";
+import { allPostsQuery } from "../../../utils/queries";
+
+
+type  Data = {
+    name: string
+}
+
+export default function handler(
+    req: NextApiRequest,
+    res:  NextApiResponse 
+) {
+
+    if(req.method === 'GET'){
+        const  query = allPostsQuery();
+    }
+    res.status(200).json({name: 'Response success'})
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from "next";
+import axios from "axios";
 
 
 const Home = () => {
@@ -8,6 +9,15 @@ const Home = () => {
     </div>
   );
 };
+
+export const getServerSideProps = async () => {
+   const response = await axios.get(`http://localhost:3000/api/post`)
+
+   console.log(response.data.name);
+   return {
+    props: {}
+   }
+}
 
 export default Home;
 
